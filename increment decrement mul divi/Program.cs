4605b1785485c88e4ff2d303e8b110d0694d95e7
ayush <ayush@AYUSHGANDHI>
2024-01-29 09:20:13 +0530

    Add project files.

---
+++ increment decrement mul divi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace increment_decrement_mul_divi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 23;
+            int p = (a % 4);
+            int b = p++;
+            int c = --b;
+            int d = (--a * 2);
+            Console.WriteLine(a);
+            Console.WriteLine(b);   
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.ReadKey();
+        }
+    }
+}
